57704528efc9d73a2c0cfc08dff0af08a6a9c9ff
HillelChen <hillel.chen@gmail.com>
2024-05-05 16:16:19 +0300

    x and o

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Button from './Components/Button/Index'
 import Or from './Components/Or/Index'
+import X from './Components/X/Index'
+import O from './Components/O/Index'
 
 export default function App() {
   return (
     <div>
       <Button children={'play solo'} />
       <Or />
+      <X />
+      <O />
 
       <Button children={'play solo'} />
     </div>
---
+++ src/Components/X/Index.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from './style.module.css'
+
+export default function X() {
+    return (
+        <div className={styles.x}>X</div>
+    )
+}
