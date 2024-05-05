f8ab79721f18dd4714492a4acd887833e68cb578
aryeh <aryehmai@gmail.com>
2024-05-05 15:26:25 +0300

    background and Box

--- src/Components/Board/index.jsx
+++ src/Components/Board/index.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
 import styles from "./style.module.css"
+import Box from '../Box'
+
 
 export default function Board() {
   return (
-    <div className={styles.Board}>board</div>
+    <div className={styles.Board}>
+      <Box/>
+      <Box/>
+      <Box/>
+      <Box/>
+      <Box/>
+      <Box/>
+      <Box/>
+      <Box/>
+      <Box/>
+    </div>
   )
 }
