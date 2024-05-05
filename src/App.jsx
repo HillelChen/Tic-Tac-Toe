fe004924317b71dd3837477a0e32e30062ec4247
HillelChen <hillel.chen@gmail.com>
2024-05-05 14:15:09 +0300

    Or finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Button from './Components/Button/Index'
+import Or from './Components/Or/Index'
 
 export default function App() {
   return (
     <div>
-      working
+      <Button children={'play solo'} />
+      <Or />
       <Button children={'play solo'} />
     </div>
   )
