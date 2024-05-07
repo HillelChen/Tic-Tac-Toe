a8e4949c7a349d084d478ca92581effd7ffe1802
aryeh <aryehmai@gmail.com>
2024-05-07 17:53:44 +0300

    handleWin function - not work yet

--- src/Components/Board/index.jsx
+++ src/Components/Board/index.jsx
@@ -1,70 +1,74 @@
-import React, { useState } from 'react'
-import styles from "./style.module.css"
-import Box from '../Box'
-import X from '../X/Index'
+import React, { useState } from "react";
+import styles from "./style.module.css";
+import Box from "../Box";
+import X from "../X/Index";
 
-
-let cnt = 2
+let cnt = 2;
 export default function Board() {
   const [gameStatus, setGameStatus] = useState([0, 1, 2, 3, 4, 5, 6, 7, 8]);
-  const handleWin = (index,gameStatus)=>{
-    console.log('handleWin');
-    let i = index
-if(gameStatus[i]==gameStatus[i+1]){
-    if(gameStatus[i]==gameStatus[i+2]||gameStatus[i]==gameStatus[i-1]){
-        console.log('win')
-    }
-}else if(gameStatus[i]==gameStatus[i-1]){
-         if(gameStatus[i]==gameStatus[i-2]){
-             console.log('win')
-         }
-        }
-else if(gameStatus[i]==gameStatus[i+3]){
-         if(gameStatus[i]==gameStatus[i+6]||gameStatus[i]==gameStatus[i-3]){
-             console.log('win')
-         }
-}
-else if(gameStatus[i]==gameStatus[i-3]){
-         if(gameStatus[i]==gameStatus[i-6]){
-             console.log('win')
-         }
-}
-else
-
-console.log('continue')
-  }
+  const [win, setWin] = useState(false);
+  const handleWin = (index, gameStatus) => {
+    console.log("handleWin:", gameStatus);
+    let i = index;
+    console.log('i:',i);
+    if (gameStatus[i] == gameStatus[i + 1]) {
+      if (
+        gameStatus[i] == gameStatus[i + 2] ||
+        gameStatus[i] == gameStatus[i - 1]
+      ) {
+        setWin(true);
+      }
+    } else if (gameStatus[i] == gameStatus[i - 1]) {
+      if (gameStatus[i] == gameStatus[i - 2]) {
+        setWin(true);
+      }
+    } else if (gameStatus[i] == gameStatus[i + 3]) {
+      if (
+        gameStatus[i] == gameStatus[i + 6] ||
+        gameStatus[i] == gameStatus[i - 3]
+      ) {
+        setWin(true);
+      }
+    } else if (gameStatus[i] == gameStatus[i - 3]) {
+      if (gameStatus[i] == gameStatus[i - 6]) {
+        setWin(true);
+      }
+    } else console.log("continue");
+  };
   const handleBoxClick = (index) => {
-   
-    if(gameStatus[index] >= 0 && gameStatus[index] <=8 ){
-      cnt=cnt+1
-
+    if (gameStatus[index] >= 0 && gameStatus[index] <= 8) {
+      cnt = cnt + 1;
+      
       const newGameStatus = [...gameStatus];
       // newGameStatus[index] = <X />;
-     cnt%2==0? newGameStatus[index] = 'x':newGameStatus[index]='o';
-      console.log(newGameStatus)
+      cnt % 2 == 0
+      ? (newGameStatus[index] = "o")
+      : (newGameStatus[index] = "x");
+      console.log(newGameStatus);
       setGameStatus(newGameStatus);
     }
-    if (cnt>6){
-      console.log('cheak if win');
-      handleWin(index,gameStatus)
+    if (cnt > 6) {
+      console.log("cheak if win");
+      handleWin(index, gameStatus);
+      console.log(win);
     }
   };
-  
-  console.log({gameStatus})
-  console.log('cnt:', cnt)
- 
+
+  console.log({ gameStatus });
+  console.log("cnt:", cnt);
 
   return (
     <div className={styles.Board}>
       {gameStatus.map((value, index) => (
-      <Box 
-        gameStatus={gameStatus} 
-        key={index} 
-        onClick={() => handleBoxClick(index)} 
-         value={gameStatus[index]=='x'?'x':gameStatus[index]=='o'?'o':''}
+        <Box
+          gameStatus={gameStatus}
+          key={index}
+          onClick={() => handleBoxClick(index)}
+          value={
+            gameStatus[index] == "x" ? "x" : gameStatus[index] == "o" ? "o" : ""
+          }
         />
       ))}
     </div>
   );
 }
-
