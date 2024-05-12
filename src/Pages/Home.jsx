811b379e36c8fb084642216f148d2679790f1665
HillelChen <hillel.chen@gmail.com>
2024-05-12 11:19:39 +0300

    start pages

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,29 @@
 import React from 'react'
+import Button from '../Components/Button/Index'
+// import styles from "./style.module.css"
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Board from '../Components/Board';
+
+
 
 export default function Home() {
     return (
-        <div>Home</div>
+        <Router>
+
+            <div className='homePage'>
+                <div>
+                    <Routes>
+                        <Route path="/board" element={<Board />} />
+                    </Routes>
+                    tic tac toe
+                </div>
+                <div className='homeButton'>
+                    <Link to='/board'>
+                        <Button>{'PLAY SOLO'}</Button>
+                    </Link>
+                    <Button>{'PLAY WITH A FRIEND'}</Button>
+                </div>
+            </div>
+        </Router>
     )
 }
