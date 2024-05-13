975e5bf066b8bd4601b78522715b5ea9536a6cfb
aryeh <aryehmai@gmail.com>
2024-05-13 09:41:05 +0300

    end of day

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ import GameBoard from "./Pages/GameBoard"
 import { SocketProvider } from "./socket"
 import ChoosePlayer from "./Pages/ChoosePlayer";
 import ChoosePlayerActive from "./Pages/ChoosePlayerActive";
-
+import JoinOrCreat from "./Pages/JoinOrCreat";
+import Waiting from './Pages/Waiting'
 
 
 
@@ -14,9 +15,11 @@ export default function App() {
     <Routes>
       <Route index element={<Home />} />
       <Route path="GameBoard" element={<GameBoard />} />
+      <Route path="JoinGame" element={<JoinOrCreat />} />
+      <Route path="waiting" element={<Waiting />} />
       <Route path="choosePlayer" element={<ChoosePlayer />} />
       <Route path="choosePlayerActive" element={<ChoosePlayerActive />} />
-
+      
     </Routes>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import { SocketProvider } from './socket.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
+        <SocketProvider>
         <App />
+        </SocketProvider>
     </BrowserRouter>
 
 )
